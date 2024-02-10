20fcc4c6477a4cde4616fd53b76f9e44c407ef3b
lynx <coeducation97@gmail.com>
2024-02-10 16:41:36 +0900

    rest parameter type and destructuring type

---
+++ 11_Rest_Parameter_And_Destructuring.js
@@ -0,0 +1,39 @@
+// rest paramter는 맨 뒤에 사용
+function sum() {
+    var a = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        a[_i] = arguments[_i];
+    }
+    console.log(a);
+}
+function 함수(_a) {
+    var student = _a.student, age = _a.age;
+    console.log(student, age);
+}
+var obj4 = { student: true, age: 20 };
+함수(obj4);
+// 숙제
+function getMax() {
+    var rest = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        rest[_i] = arguments[_i];
+    }
+    var max = 0;
+    rest.forEach(function (num) {
+        if (num > max) {
+            max = num;
+        }
+    });
+    return max;
+}
+console.log(getMax(4, 6, 3, 2));
+function func7(_a) {
+    var user = _a.user, comment = _a.comment, admin = _a.admin;
+    console.log(user, comment, admin);
+}
+func7({ user: 'kim', comment: [3, 5, 4], admin: false });
+function func8(_a) {
+    var a = _a[0], b = _a[1], c = _a[2];
+    console.log(a, b, c);
+}
+func8([40, 'wine', false]);
---
+++ 11_Rest_Parameter_And_Destructuring.ts
@@ -0,0 +1,41 @@
+// rest paramter는 맨 뒤에 사용
+function sum(...a: number[]) {
+  console.log(a);
+}
+
+
+// 함수 파라미터에 destructuring 사용
+// 오브젝트 안에 있던 자료를 파라미터로 넣고 싶다
+
+interface ObjType2 {
+  student: boolean;
+  age: number;
+}
+function 함수({ student, age }: ObjType2) {
+  console.log(student, age);
+}
+let obj4 = { student: true, age: 20 };
+함수(obj4);
+
+
+// 숙제
+function getMax(...rest: number[]): number {
+  let max = 0;
+  rest.forEach(num => {
+    if (num > max) {
+      max = num;
+    }
+  })
+  return max;
+}
+console.log(getMax(4, 6, 3, 2));
+
+function func7({user, comment, admin}: {user:string, comment: number[], admin: boolean}):void {
+  console.log(user, comment, admin);
+}
+func7({ user : 'kim', comment : [3,5,4], admin : false });
+
+function func8([a, b, c]: (number|string|boolean)[]){
+  console.log(a, b, c);
+}
+func8([40, 'wine', false]);
