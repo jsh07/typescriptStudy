7b63d197b83b5b108e3df4b50e68c3716dbbf4ad
lynx <coeducation97@gmail.com>
2024-02-10 10:37:49 +0900

    literal types

---
+++ 06_Literal_Types.ts
@@ -0,0 +1,22 @@
+// Literal Types
+// 사전에 정의한 타입만 넣을 수 있도록 지정할 수 있음
+// - 변수에 들어올 값 엄격하게 관리
+// - 자동완성 기능
+let itsMe: 'Woman' | '솔로';
+itsMe = 'Woman';
+
+// 왜 에러남?
+let data = {
+  name: "kim"
+} as const
+
+function func5(a: "kim"){ // kim 이라는 자료만 들어올 수 있음 X
+                          // kim 이라는 타입만 들어올 수 있음 O
+}
+func5(data.name); // error, data.name은 "kim"이라는 타입이 아님
+
+
+// 해결책 : as const 키워드
+// 1. object value 값을 그대로 타입으로 지정
+// 2. object 속성들에 모두 readonly를 붙여줌
+// => object 자료 완전히 잠그기
