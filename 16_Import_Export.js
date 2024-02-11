417e1cd8844aedec54f23b862a4bf306e8dfd0bd
lynx <coeducation97@gmail.com>
2024-02-11 19:08:46 +0900

    import/export, module, namespace

---
+++ 16_Import_Export.js
@@ -0,0 +1,3 @@
+// import { 이름, UserType, NameType } from './ForImport';
+///<reference path="./ForImport.ts" />
+// let a:네임스페이스.Name = "str";
---
+++ ForImport.ts
@@ -0,0 +1,10 @@
+// export let 이름 = "kim";
+// export type NameType = string;
+// export interface UserType { name: string; age: number };
+
+// 옛날에 어떻게 import/export?
+// 타입 변수 숨기기 문법 사용 
+// => module(더 옛날), namespace 키워드를 사용해 object 자료 안에 숨겼음
+// module 네임스페이스 {
+//   export type Name = string | number;
+// }
