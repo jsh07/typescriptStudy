6f4309f8cdb99ffc3881cc7bd35e06f44f1781ad
lynx <coeducation97@gmail.com>
2024-02-11 20:55:05 +0900

    generic

---
+++ 17_Generic.js
@@ -0,0 +1,49 @@
+// Generic
+function func12(x) {
+    return x[0];
+}
+var a = func12([4, 2]);
+var b = func12(["4", "2"]);
+var c = func12(["4", "2"]); // 타입 안적어줘도 됨
+console.log(c);
+// 타입파라미터 제한두기
+// extends : narrowing 인정해줌
+function func13(x) {
+    return x - 1;
+}
+var d = func13(100);
+console.log(d); // 99
+function func14(x) {
+    return x.length;
+}
+var e = func14("hello");
+var f = func14(["a", "b", "c"]);
+console.log(e);
+console.log(f);
+// class
+var GenericClass = /** @class */ (function () {
+    function GenericClass(a) {
+        this.x = a;
+    }
+    return GenericClass;
+}());
+var instance = new GenericClass("aaa");
+console.log(instance.x);
+var me = { name: "jeon", age: 28 };
+// 숙제
+function func15(a) {
+    return a.length;
+}
+var data2 = '{"name" : "dog", "age": 1 }';
+function func16(json) {
+    return JSON.parse(json);
+}
+console.log(func16(data2));
+var Person4 = /** @class */ (function () {
+    function Person4(a) {
+        this.name = a;
+    }
+    return Person4;
+}());
+var aaa = new Person4("어쩌구");
+aaa.name;
---
+++ 17_Generic.ts
@@ -0,0 +1,80 @@
+// Generic
+function func12<MyType>(x: MyType[]): MyType {
+  return x[0];
+}
+
+let a = func12<number>([4,2]);
+let b = func12<string>(["4", "2"]);
+let c = func12(["4", "2"]); // 타입 안적어줘도 됨
+console.log(c);
+
+// 타입파라미터 제한두기
+// extends : narrowing 인정해줌
+function func13<MyType extends number>(x: MyType) {
+  return x - 1;
+}
+let d = func13<number>(100); 
+console.log(d);// 99
+
+// length 속성의 타입이 number인 타입이 들어오면~~
+// string, Array(string[], number[], ...) 등
+interface LengthCheck {
+  length: number
+}
+function func14<MyType extends LengthCheck> (x: MyType) {
+  return x.length;
+}
+let e = func14<string>("hello");
+let f = func14<string[]>(["a", "b", "c"]);
+console.log(e);
+console.log(f);
+
+// class
+class GenericClass<T> {
+  x: T;
+  constructor(a: T) {
+    this.x = a;
+  }
+}
+
+let instance = new GenericClass<string>("aaa");
+console.log(instance.x);
+
+// type
+type NewType<T> = {
+  name: T;
+  age: T;
+}
+
+// interface 
+interface NewInterface<S, N> {
+  name: S;
+  age: N;
+}
+let me:NewInterface<string, number> = { name: "jeon", age: 28 }
+
+
+// 숙제
+function func15<T extends string | string[]>(a: T) {
+  return a.length;
+}
+
+interface Animal2 {
+  name: string;
+  age: number;
+}
+let data2 = '{"name" : "dog", "age": 1 }';
+function func16<T extends Animal2>(json:string):T {
+  return JSON.parse(json);
+}
+console.log(func16<Animal2>(data2));
+
+class Person4<T> {
+  name: T;
+  constructor(a: T){
+    this.name = a;
+  }
+}
+let aaa = new Person4("어쩌구");
+aaa.name;
+
