d7248dc2434fe8ac23dffaacb0f8e255b5824d32
lynx <coeducation97@gmail.com>
2024-02-15 16:47:56 +0900

    d.ts 파일

---
+++ 20_Declare_File.js
@@ -0,0 +1,6 @@
+"use strict";
+// ~.d.ts 파일
+// 타입 정의 보관용 파일
+// import / export
+// 많으면 
+Object.defineProperty(exports, "__esModule", { value: true });
---
+++ 20_Declare_File.ts
@@ -0,0 +1,9 @@
+// ~.d.ts 파일
+// 타입 정의 보관용 파일
+// import / export
+// 많으면 
+
+// 모든 타입을 정리해놓은 레퍼런스용으로 d.ts 파일 사용
+
+export type Age = number;
+export interface Person { name : string }
